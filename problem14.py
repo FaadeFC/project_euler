8e24b5e73f418a5e0aab3a78db990c48611ae3f0
Jordan <faadefc@gmail.com>
2024-09-08 12:32:09 -0700

    Solve Problem 14 from Project Euler.

---
+++ problem14.py
@@ -0,0 +1,29 @@
+def collatz(n, i=1):
+    """ Return the length of sequence from the Collatz sequence. """
+    if n == 1:
+        return i
+    elif n % 2 == 0:
+        return collatz(n / 2, i + 1)
+    else:
+        return collatz(3 * n + 1, i + 1)
+
+def chain(n):
+    """ Return the longest chain with starting number under n. """
+    number = maximum = 1
+
+    for i in range(1, n):
+        chain = collatz(i)
+
+        if chain > maximum:
+            maximum = chain
+            number = i
+    
+    return number
+
+def main():
+    """ Execute the main program. """
+    n = 1000000
+    answer = chain(n)
+    print(answer)
+
+main()
