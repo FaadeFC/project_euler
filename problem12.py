d2d63ae9a74d3f5275a5d9831a39c3e5ac160c29
Jordan <faadefc@gmail.com>
2024-09-07 21:11:16 -0700

    Finish Problem 12 from Project Euler.

--- problem12.py
+++ problem12.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def triangular(n):
     """ Return the nth triangular number. """
     answer = n * (n + 1) / 2
@@ -8,9 +10,12 @@ def divisors(n):
     """ Return the number of factors that n has. """
     total = 0
 
-    for i in range(1, n + 1):
+    for i in range(1, int(sqrt(n)) + 1):
         if n % i == 0:
-            total += 1
+            if sqrt(n) == i:
+                total += 1
+            else:
+                total += 2
     
     return total
 
@@ -23,7 +28,6 @@ def main():
         number = triangular(i)
         total = divisors(number)
         i += 1
-        print(number, total)
     
     print(number)
 
